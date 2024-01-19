9f2a72e5cec17b18749352ad7d9d6cd19d204d5b
Dmitry <dimchik843@gmail.com>
2024-01-19 19:19:56 +0300

    redact router

--- router/mountRouter.js
+++ router/mountRouter.js
@@ -1,5 +1,5 @@
 const docRouter = require('./document-router/doc-router')
 
 module.exports = (app) => {
-    app.use('/api-postDocument', docRouter)
+    app.use('/api-postDocument-Roma', docRouter)
 }
